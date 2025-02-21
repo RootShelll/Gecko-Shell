2056e3467095b6c1a2b7dc01acd172e66b26787c
R00t-Shell <RootShell@r00t-shell.com>
2025-02-21 17:23:18 +0300

    Update Gecko-Shell.php

--- Gecko-Shell.php
+++ Gecko-Shell.php
@@ -4,7 +4,7 @@
     |  Author: R00t Shell                                                             |
     |  Date: 2025-02-18 06:45:35                                                      |
     |  Website: https://r00t-shell.com                                                |
-    |  Virus Total: a5ae0aab352871bc0b038b3aa43b03eb223425628c884af7b9fc592cd34eb86c  |
+    |  Virus Total: d06df3bf9775e2fc7d824aab21ef926b90dc7632ce5ec00fc0d33c47ebe8d8ce  |
     |  Description: Obfuscates PHP code to increase security and protect source code. |
     |_________________________________________________________________________________|
 */
